e10ef8340d71d9b028044b21f6f1b02239b83caa
Vishwajeet Srivastava <vs0397297@gmail.com>
2026-06-19 17:42:50 +0530

    Added new features and fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,20 +49,24 @@ const Navbar = () => {
           alt=""
         />
         <div className="group relative">
-          <Link to="/login">
+          
             <img
+              onClick={()=> token ? null : navigate("/login")}
               className="w-5 cursor-pointer"
               src={assets.profile_icon}
               alt=""
             />
-          </Link>
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+           {/* Dropdown Menu */}
+
+          {token &&
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profiles</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p onClick={()=>navigate("/orders")} className="cursor-pointer hover:text-black">Orders</p>
               <p onClick={logout} className="cursor-pointer hover:text-black">LogOut</p>
             </div>
           </div>
+          }
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -17,7 +17,7 @@ const ShopContextProvider = (props) => {
   const [token,setToken] = useState("")
   const navigate = useNavigate();
 
-  const addToCart = (itemId, size) => {
+  const addToCart = async (itemId, size) => {
     if (!size) {
       toast.error("Select Product Size");
       return;
@@ -37,6 +37,15 @@ const ShopContextProvider = (props) => {
     }
 
     setCartItems(cartData);
+
+    if(token){
+      try {
+        await axios.post(backendUrl + "/api/cart/add", {itemId,size}, {headers:{token}})
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+      }
+    }
   };
 
   const getCartCount = () => {
@@ -59,6 +68,17 @@ const ShopContextProvider = (props) => {
     cartData[itemId][size] = quantity;
 
     setCartItems(cartData);
+    
+    if(token){
+      try {
+        await axios.post(backendUrl + "/api/cart/update",{itemId, size, quantity},{headers:{token}})
+
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+      }
+    }
+
   };
 
   const getCartAmount = () => {
@@ -90,6 +110,18 @@ const ShopContextProvider = (props) => {
     }
   }
 
+  const getUserCart = async (token) => {
+      try {
+        const response = await axios.post(backendUrl + "/api/cart/get",{},{headers:{token}})
+        if(response.data.success){
+          setCartItems(response.data.cartData)
+        }
+      } catch (error) {
+        console.log(error)
+        toast.error(error.message)
+      }
+  }
+
   useEffect(() => {
      getProductsData()
   },[])
@@ -97,6 +129,7 @@ const ShopContextProvider = (props) => {
   useEffect(() => {
      if(!token && localStorage.getItem("token")){
          setToken(localStorage.getItem("token"))
+         getUserCart(localStorage.getItem("token"))
      }
   },[])
 
