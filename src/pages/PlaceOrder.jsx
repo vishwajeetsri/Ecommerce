82906794be843a305d8e2a2ed2e5eff05452642c
Vishwajeet Srivastava <vs0397297@gmail.com>
2026-06-22 17:52:22 +0530

    Added new features and fixes

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -3,12 +3,95 @@ import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const PlaceOrder = () => {
   const [method, setMethod] = useState("cod");
-  const { navigate } = useContext(ShopContext);
+  const { navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, delivery_fee, products } = useContext(ShopContext);
+  
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+
+    setFormData(data => ({...data,[name]:value}))
+  }
+  
+  const onSubmitHandler = async (event) => {
+     event.preventDefault()
+     try {
+       let orderItems = []
+
+      for (const productId in cartItems) {
+  for (const size in cartItems[productId]) {
+    if (cartItems[productId][size] > 0) {
+      const itemInfo = structuredClone(
+        products.find(product => product._id === productId)
+      );
+
+      if (itemInfo) {
+        itemInfo.size = size;
+        itemInfo.quantity = cartItems[productId][size];
+        orderItems.push(itemInfo);
+      }
+    }
+  }
+}
+
+let orderData = {
+  address: formData,
+  items: orderItems,
+  amount: getCartAmount() + delivery_fee
+
+}
+
+switch(method){
+  //API calls for COD
+  case "cod":
+    const response = await axios.post(backendUrl + "/api/order/place",orderData,{headers:{token}})
+    if(response.data.success){
+      setCartItems({})
+      navigate("/orders")
+    }else{
+      toast.error(response.data.message)
+    }
+    break;
+    
+    case "stripe":
+      const responseStripe = await axios.post(backendUrl + "/api/order/stripe",orderData,{headers:{token}} )
+      if(responseStripe.data.success){
+        const {session_url} = responseStripe.data
+        window.location.replace(session_url)
+      }else{
+        toast.error(responseStripe.data.message)
+      }
+    break;
+    default:
+      break;  
+}
+       
+     } catch (error) {
+      console.log(error);
+      toast.error(error.messsage)
+     }
+  }
+
+  
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+    <form onSubmit={onSubmitHandler} className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
       {/*---------------Left Side --------------*/}
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
         <div className="text-xl sm:text-2xl my-3">
@@ -17,22 +100,30 @@ const PlaceOrder = () => {
 
         <div className="flex gap-3">
           <input
+            required
+            onChange={onChangeHandler} name="firstName" value={formData.firstName}
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
             type="text"
             placeholder="First name"
           />
-          <input
+          <input 
+            required
+            onChange={onChangeHandler} name="lastName" value={formData.lastName}
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
             type="text"
             placeholder="Last name"
           />
         </div>
         <input
+          required
+          onChange={onChangeHandler} name="email" value={formData.email}
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
           type="email"
           placeholder="Email address"
         />
         <input
+          required
+          onChange={onChangeHandler} name="street" value={formData.street}
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
           type="text"
           placeholder="Street"
@@ -40,11 +131,15 @@ const PlaceOrder = () => {
 
         <div className="flex gap-3">
           <input
+            required
+            onChange={onChangeHandler} name="city" value={formData.city}
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
             type="text"
             placeholder="City"
           />
           <input
+            required
+            onChange={onChangeHandler} name="state" value={formData.state}
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
             type="text"
             placeholder="State"
@@ -53,17 +148,23 @@ const PlaceOrder = () => {
 
         <div className="flex gap-3">
           <input
+            required
+            onChange={onChangeHandler} name="zipcode" value={formData.zipcode}
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
             type="number"
             placeholder="Zip Code"
           />
           <input
+            required
+            onChange={onChangeHandler} name="country" value={formData.country}
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
             type="text"
             placeholder="Country"
           />
         </div>
         <input
+          required
+          onChange={onChangeHandler} name="phone" value={formData.phone}
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full "
           type="number"
           placeholder="Phone"
@@ -119,8 +220,7 @@ const PlaceOrder = () => {
           </div>
 
           <div className="w-full text-end mt-8">
-            <button
-              onClick={() => navigate("/orders")}
+            <button type="submit"
               className="bg-black text-white px-16 py-3 text-sm"
             >
               PLACE ORDER
@@ -128,7 +228,7 @@ const PlaceOrder = () => {
           </div>
         </div>
       </div>
-    </div>
+    </form> 
   );
 };
 
