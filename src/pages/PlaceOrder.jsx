2df80a51e132d67b2550b05bea2f8980718877a6
Vishwajeet Srivastava <vs0397297@gmail.com>
2026-06-23 19:56:23 +0530

    Added new features and fixes

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -26,8 +26,35 @@ const PlaceOrder = () => {
   const onChangeHandler = (event) => {
     const name = event.target.name
     const value = event.target.value
-
     setFormData(data => ({...data,[name]:value}))
+  }
+
+  const initPay = (order) => {
+     const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+        amount: order.amount,
+        currency: order.currency,
+        name: "Order Payment",
+        description: "Order Payment",
+        order_id: order.id,
+        receipt: order.receipt,
+        handler: async (response) => {
+              console.log(response);
+              try {
+                const {data} = await axios.post(backendUrl + "/api/order/verifyRazorpay",response,{headers:{token}})
+                if(data.success) {
+                  navigate("/orders")
+                  setCartItems({})
+                }
+              } catch (error) {
+                console.log(error);
+                toast.error(error)
+                
+              }
+        } 
+     }
+     const rzp = new window.Razorpay(options)
+     rzp.open()
   }
   
   const onSubmitHandler = async (event) => {
@@ -79,6 +106,13 @@ switch(method){
         toast.error(responseStripe.data.message)
       }
     break;
+    case "razorpay":
+       const responseRazorpay = await axios.post(backendUrl + "/api/order/razorpay", orderData, {headers:{token}})
+       if(responseRazorpay.data.success){
+          initPay(responseRazorpay.data.order)
+          
+       }
+     
     default:
       break;  
 }
